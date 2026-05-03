b3b1d0bede1296b8afb5c51f9e858379399d5c10
Mauro Lopez <mauro00lopez99@gmail.com>
2026-05-02 21:54:05 -0300

    Resuelve problema 5

--- Dsw2026Ej8/Problema3.cs
+++ Dsw2026Ej8/Problema3.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Dsw2026Ej8
 {
-    internal class Problema3
+    public class Problema3
     {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaValor = originalValue;
+            copiaValor++;
+
+            var copiaProducto = product;
+            copiaProducto.SetDescription("Producto Modificado");
+
+            return $"{originalValue}-{copiaValor}-{product.GetDescription()}";
+        }
     }
 }
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,8 +4,25 @@
     {
         static void Main(string[] args)
         {
-            var helper = new ProductHelper();
-            Console.WriteLine(helper.ObtenerEtiquetaProducto(1, "Teclado", 19.99m));
+            /*var helper = new ProductHelper();
+            Console.WriteLine(helper.ObtenerEtiquetaProducto(1, "Teclado", 19.99m));*/
+
+            /*var p = new Problema2();
+            Console.WriteLine(p.CrearResumenVenta(100, "Mouse", 2, 1500));*/
+
+            /*var prod = new Product("Mouse");
+            var p3 = new Problema3();
+
+            Console.WriteLine(p3.CompararCopias(10, prod));*/
+
+            var p5 = new Problema5();
+
+            Sale s1 = new RetailSale(1000);
+            Sale s2 = new WholesaleSale(1000);
+
+            Console.WriteLine(p5.ObtenerImporteFinal(s1));
+            Console.WriteLine(p5.ObtenerImporteFinal(s2)); 
+
         }
     }
 }
